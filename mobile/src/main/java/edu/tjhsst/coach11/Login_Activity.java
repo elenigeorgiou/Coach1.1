6d4115ecbcf7b72ed287633a0e89114bc1ffaae4
Eleni Georgiou <helenig12@gmail.com>
2016-10-26 13:58:19 -0400

    Basic Draft of the app: login page and heart rate monitor.

--- mobile/src/main/java/edu/tjhsst/coach11/Login_Activity.java
+++ mobile/src/main/java/edu/tjhsst/coach11/Login_Activity.java
@@ -1,25 +1,30 @@
 package edu.tjhsst.coach11;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.View;
 
 
-public class HeartRateTestActivity extends ActionBarActivity {
-    private TextView heartrate;
+public class Login_Activity extends ActionBarActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_heart_rate_test);
-        heartrate = (TextView) findViewById(R.id.instantHearRate); //sets heartrate = to the label; will be used to update
-    }
+        setContentView(R.layout.activity_login_);
 
+    }
+    public void buttonClickFunction(View v)
+    {
+        Intent intent = new Intent(getApplicationContext(), HeartRateTestActivity.class); //takes user from Login page to Heart Rate Test Activity
+        startActivity(intent);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_heart_rate_test, menu);
+        getMenuInflater().inflate(R.menu.menu_login_, menu);
         return true;
     }
 
